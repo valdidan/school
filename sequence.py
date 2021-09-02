8e07181a9df4e5e523768214783be19a74d1000e
valdidan <valdidanielsson@gmail.com>
2021-09-02 14:10:24 +0000

    second try

--- sequence.py
+++ sequence.py
@@ -2,7 +2,7 @@ from typing import Sequence
 
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
-
+seq = 0
 for i in range(n):
-    seq = n + seq
+    seq = i + seq
     print(seq)
