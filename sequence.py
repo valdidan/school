30d0f1abcff222a37a1ecd41081633b179deca42
valdidan <valdidanielsson@gmail.com>
2021-09-02 14:31:40 +0000

    first 4 ok

--- sequence.py
+++ sequence.py
@@ -1,8 +1,20 @@
-from typing import Sequence
-
-
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+seq1 = 0
+seq2 = 0
+seq3 = 0
 seq = 0
 for i in range(n):
-    seq = i + seq
-    print(seq)
+    if i == 0:
+        print(i + 1)
+        seq1= i + 1 
+    if i == 1:
+        print(i + 1)
+        seq2 = i +1 
+    elif i > 1:
+        seq3 = seq1 + seq2  + seq3
+        seq = seq3
+        print(seq)
+
+
+     
+    
